6ca11ccf2f0871692a424fc16234e013765c6276
eeLude <153170986+eeLude@users.noreply.github.com>
2023-12-07 10:28:43 +0200

    Add files via upload

---
+++ result.php
@@ -0,0 +1,5 @@
+<?php
+echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+echo "Your information was sent<br><br>";
+echo '<a style="color:#a90d7f" href="index.html">Back to Home</a>';
+?>
